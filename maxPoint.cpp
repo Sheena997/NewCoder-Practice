38dbc3ecb18daf69df4e0eaa68a214e17021c4bd
Sheena爽 <18365368008@163.com>
2020-07-08 14:56:22 +0800

    施爽：于2020年7月8日上传的在二维平面整数点集中求最大点集（sort和lambda）的代码。

--- maxPoint.cpp
+++ maxPoint.cpp
@@ -1,24 +1,39 @@
 /*
-P为给定的二维平面整数点集。定义 P 中某点x，如果x满足 P 中任意点都不在 x 的右上方区域内（横纵坐标都大于x），则称其为“最大的”。求出所有“最大的”点的集合。（所有点的横坐标和纵坐标都不重复, 坐标轴范围在[0, 1e9) 内）
+链接：https://ac.nowcoder.com/questionTerminal/ea209d6014c44763a41ee04a11bf2ef1?toCommentId=1682672
+来源：牛客网
 
+[编程题]编程题1
+P为给定的二维平面整数点集。定义 P 中某点x，如果x满足 P 中任意点都不在 x 的右上方区域内（横纵坐标都大于x），
+则称其为“最大的”。求出所有“最大的”点的集合。（所有点的横坐标和纵坐标都不重复, 坐标轴范围在[0, 1e9) 内）
 如下图：实心点为满足条件的点的集合。请实现代码找到集合 P 中的所有 ”最大“ 点的集合并输出。
 
+输入描述:
+第一行输入点集的个数 N， 接下来 N 行，每行两个数字代表点的 X 轴和 Y 轴。
+对于 50%的数据,  1 <= N <= 10000;
+对于 100%的数据, 1 <= N <= 500000;
 
 
-思路：
-
-对于所有的点先按照y从大到小排序O(NlongN)
-
-从大到小遍历排好序的点集，当前y是出现过的最大的y，即是需要的结果，进行输出O(N)。
-
-总体时间复杂度为O(NlogN)
+输出描述:
+输出“最大的” 点集合， 按照 X 轴从小到大的方式输出，每行两个数字分别代表点的 X 轴和 Y轴。
+示例1
+输入
+5
+1 2
+5 3
+4 6
+7 5
+9 0
+输出
+4 6
+7 5
+9 0
 */
 #include <iostream>
 #include <vector>
-#include <algorithm>>
- 
+#include <algorithm>
+
 using namespace std;
- 
+
 int main()
 {
     int n = 0;
@@ -28,24 +43,23 @@ int main()
         for(int i = 0; i < n; ++i)
         {
             int x = 0, y = 0;
-            cin >> x >> y;
+            scanf("%d%d", &x, &y);
             vp.push_back(make_pair(x, y));
         }
-         
+        
         sort(vp.begin(), vp.end(), [](pair<int, int>& p1, pair<int, int>& p2)->bool{
             return p1.second > p2.second;
         });
-         
-        int max = -1;
-        for(pair<int, int>& e : vp)
+        
+        int maxNum = -1;
+        for(pair<int, int> e : vp)
         {
-            if(e.first > max)
+            if(e.first > maxNum)
             {
-                cout << e.first << " " << e.second << endl;
-                max = e.first;
+                printf("%d %d\n", e.first, e.second);
+                maxNum = e.first;
             }
         }
     }
-     
     return 0;
 }
