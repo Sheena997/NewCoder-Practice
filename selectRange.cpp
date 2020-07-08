59b3127981851eec767c7a3ff88d4a93401f3673
Sheena爽 <18365368008@163.com>
2020-07-08 16:57:48 +0800

    施爽：于2020年7月8日上传的求数组区间中的最大值（用set排序）的代码。

--- selectRange.cpp
+++ selectRange.cpp
@@ -23,41 +23,40 @@
 */
 #include <iostream>
 #include <vector>
-#include <set>
 
 using namespace std;
 
 int main()
 {
-	int n = 0;
-	while (cin >> n)
-	{
-		vector<int> vc(n, 0);
-		for (auto& e : vc)
-			cin >> e;
-		set<int> s(vc.begin(), vc.end());
-		long long res = 0;
-
-		for (int c : s)
-		{
-			int cur = 0, maxNum = -1;
-			for (int i = 0; i < n; ++i)
-			{
-				if (vc[i] >= c)
-				{
-					cur += vc[i];
-					maxNum = max(maxNum, cur);
-				}
-				else
-				{
-					cur = 0;
-				}
-			}
-			res = res > maxNum * c ? res : maxNum * c;
-		}
-
-		cout << res << endl;
-	}
-
-	return 0;
+    int n = 0;
+    while(cin >> n)
+    {
+        vector<int> vc(n, 0);
+        for(auto& e : vc)
+            cin >> e;
+        long long res = 0;
+        for(int i = 0; i < n; ++i)
+        {
+            int cur = 0, maxNum = -1;
+            for(int j = i; j < n; ++j)
+            {
+                if(vc[j] >= vc[i])
+                    cur += vc[j];
+                else
+                    break;
+            }
+            for(int j = i - 1; j >= 0; --j)
+            {
+                if(vc[j] >= vc[i])
+                    cur += vc[j];
+                else
+                    break;
+            }
+            cur *= vc[i];
+            res = res > cur ? res : cur;
+        }
+        cout << res << endl;
+    }
+    
+    return 0;
 }
